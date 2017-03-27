0b81f7fd6823b58bad31a2365d9ea404230c842b
Arun <prith000@citymail.cuny.edu>
2017-03-27 11:05:25 -0400

    reorgnize directories

---
+++ src/index.js
@@ -0,0 +1,89 @@
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+let apiUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+
+let newQuote = {
+    content: '',
+    title: ''
+};   
+
+function fetchQuotes() {
+$.ajax( {
+    url: apiUrl,
+    success: function(data) {
+        var quote = data.shift(); 
+        $('#quote-title').text(quote.title);
+        $('#quote-content').html(quote.content);
+        $('#tweet-button').html(
+            '<a target="_blank" href="https://twitter.com/intent/tweet?hashtags=quotes&related=design&text=' + quote.content + '" >Tweet</a>'
+        );
+        $('#facebook-button').html(
+            '<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=' + quote.content + '" >Share</a>'
+        );
+
+        newQuote['content'] = quote.content;
+        newQuote['title'] = quote.title;
+    },
+    cache: false
+});
+}
+// creat quote component
+//var QuoteComponent = React.createClass({
+class QuoteComponent extends React.Component{
+    constructor(props){
+        super(props); 
+        this.state = {
+            content: props.Quote.content,
+            title: props.Quote.title
+        };
+
+        this.componentDidMount = this.componentDidMount.bind(this);
+    }
+    
+    componentDidMount() {
+        axios.get(apiUrl)
+            .then(res => {
+                var Quotes = res.data;
+                this.setState({  
+                    content: Quotes.content,
+                    title: Quotes.title
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    render(){
+        return(
+            <div className="card-block">
+                <h3 id="quote-content" className="card-title">{this.state.content}</h3> 
+                <span>— </span>
+                <span id="quote-title" className="card-text">{this.state.title}</span>
+                <div className="d-flex justify-content-start mt-3">
+                    <button id="tweet-button" className="btn btn-link"></button>
+                    <button id="facebook-button" className="btn btn-link"></button>
+                    <button id="new-quote-button" onClick={fetchQuotes} className="btn btn-primary ml-auto">New Quote</button>
+                </div>
+            </div>
+        )
+    }//end render
+
+}
+
+
+
+fetchQuotes();
+// put quote component into html page
+ReactDOM.render(
+    <QuoteComponent Quote={newQuote} />, 
+    document.getElementById('quote-wrapper')
+);
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ var path = require('path');
 
 module.exports = {
 
-    entry: path.resolve(__dirname, 'src') + '/app/index.js',
+    entry: path.resolve(__dirname, 'src') + '/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist') + '/app',
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/app/'
     },
@@ -12,6 +12,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
+                exclude: /(node_modules)/,
                 include: path.resolve(__dirname, 'src'),
                 loader: 'babel-loader',
                 query: {
